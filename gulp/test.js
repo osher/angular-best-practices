af2b51c185ac7cf2c87e68455ff90b1fd5d060c2
osher <osher.filter@gmail.com>
2015-12-27 17:24:08 +0200

    windows compatibility

--- gulp/test.js
+++ gulp/test.js
@@ -1,11 +1,12 @@
+var path = require('path');
 var gulp = require('gulp');
 var karma = require('karma').server;
 var isTravis = process.env.TRAVIS || false;
-var pathToKarmaConf = __dirname.replace('/gulp', '');
+var pathToKarmaConf = process.cwd();
 
 module.exports = gulp.task('test', function (done) {
   karma.start({
-    configFile: pathToKarmaConf + '/karma.conf.js',
+    configFile: path.join(pathToKarmaConf ,'karma.conf.js'),
     singleRun: isTravis
   }, done);
 });
